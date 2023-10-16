356863b3e60ae96738a8f256591d9ab7c3c68c56
jarvisbot01 <jdrodriguez653@soy.sena.edu.co>
2023-10-16 17:12:57 +0000

    :sparkles: feat(Api): implement JWT authentication, user service, and helpers
    - Add JWT and Authorization helpers for JWT token configuration and user roles.
    - Introduce IUserService and UserService for user-related operations.
    - Introduce ApplicationServiceExtension.cs to configure JWT authentication.
    - Update Api.csproj and Program.cs to include the new extension.

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,8 +1,12 @@
+using Api.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddApplicationServices();
+builder.Services.AddJwt(builder.Configuration);
 
 var app = builder.Build();
 
