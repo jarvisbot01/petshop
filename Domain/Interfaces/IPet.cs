26d9d3d9bc80ef3ae50c437860466a79ec18059a
jarvisbot01 <jdrodriguez653@soy.sena.edu.co>
2023-10-19 23:51:24 -0500

    :sparkles: feat(Pets): add consulta 6B
    - Interface
    - Repository
    - Controller

--- Domain/Interfaces/IPet.cs
+++ Domain/Interfaces/IPet.cs
@@ -8,5 +8,6 @@ namespace Domain.Interfaces
         Task<IEnumerable<Pet>> Consulta6A();
         Task<IEnumerable<Pet>> Consulta1B();
         Task<object> Consulta3B();
+        Task<object> Consulta6B();
     }
 }
